44a3608932a115c046d7b8668a46fd93e1501d90
rubhanazeem <rubhanazeem@gmail.com>
2019-02-12 12:25:10 +0100

    response in json

--- app/controllers/menu_controller.rb
+++ app/controllers/menu_controller.rb
@@ -1,4 +1,37 @@
 class MenuController < ApplicationController
+  include MenuHelper
+
   def today
+    response = get_todays_menu
+    if response[:success]
+      @html = response[:html].to_html
+    else
+      @html = response[:error]
+    end
+
+    respond_to do |format|
+      format.html do
+        @html = @html
+      end
+
+      format.json do
+        if response[:success]
+          h = {}
+          h[:day] = response[:html][0].text
+          h[:products] = []
+          response[:html][1..-1].each do |node|
+            product = {}
+            product[:name] = node.search('.cobie-product-name').text
+            product[:sup] = node.search('sup').text
+            product[:price] = node.search('.price').text
+            product[:description] = node.search('.cobie-product-description').text
+            h[:products] << product
+          end
+          render status: :ok, json: h.to_json
+        else
+          render status: :conflict, json: response[:error].to_json
+        end
+      end
+    end
   end
 end
--- app/helpers/menu_helper.rb
+++ app/helpers/menu_helper.rb
@@ -1,2 +1,17 @@
 module MenuHelper
+  require 'open-uri'
+
+  def get_todays_menu
+    begin
+      doc = Nokogiri::HTML(open("https://www.cobie.de/speisekarte/"))
+      html = doc.css('section.page__content div.vc_row.wpb_row.vc_row-fluid div.foodCarousel div.item')[0].children
+      success = true
+      error = nil
+    rescue => e
+      html = nil
+      success = false
+      error = e
+    end
+    {html: html, success: success, error: error}
+  end
 end
